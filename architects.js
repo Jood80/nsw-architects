cff7b849e5b88df41f0aa937fb498c67c773bcb1
Jood80 <shawar.nujood@gmail.com>
2021-11-24 14:32:46 +0200

    Feat(*): get the details of a single architect

--- architects.js
+++ architects.js
@@ -9,16 +9,34 @@ const architects = {
 
   initialize: async (limit) => {
     architects.browser = await puppeteer.launch({
-      headless: true
+      headless: false
     })
     architects.page = await architects.browser.newPage()
-    await architects.page.goto(BASE_URL(limit), { waitUntil: 'networkidle2' })
-    // await architects.page.waitForTimeout(10000)
+    await architects.page.goto(BASE_URL(limit), { waitUntil: 'domcontentloaded' })
   },
 
   getLinks: async (selector) => {
     let links = await architects.page.$$eval(selector, x => x.map(x => x.firstElementChild.firstElementChild.href))
     return links
+  },
+
+  getDetails: async (url) => {
+    await architects.page.goto(url, { waitUntil: 'load' })
+
+    const name = await architects.page.evaluate(() => document.querySelector('#system > h2').textContent)
+    const info = await architects.page.$$eval("tr", x => x.filter(x => x.textContent.trim()).map(x => {
+      let key = x.textContent.split(":")[0];
+      let value = x.textContent.split(":")[1];
+      return (key === "Social Networks") ? {
+        [key]: x.lastElementChild.lastElementChild.href
+      } : { [key]: value }
+    }))
+    const details = Object.assign({}, ...info);
+
+    return {
+      name,
+      ...details,
+    }
   },
 
   close: async () => {
--- index.js
+++ index.js
@@ -2,30 +2,16 @@ const fs = require('fs/promises')
 const scrapeArch = require('./architects')
 
 const main = async () => {
-  let res = []
-  let limit = 30
-  while (limit <= 150) {
-    await scrapeArch.initialize(limit)
-    links = await scrapeArch.getLinks('tbody tr')
-    res.push(links)
-    limit += 30
-  }
+  let limit = 1;  // TODO: update the limit 
+  await scrapeArch.initialize(limit)
+  links = await scrapeArch.getLinks('tbody tr')
+  let details = await scrapeArch.getDetails(links[0])
 
-  await fs.writeFile('architects_urls.json', JSON.stringify([].concat(...res), null, 2))
+  await fs.writeFile('architect_details.json', JSON.stringify(details))
   await scrapeArch.close()
 }
 
 main()
 
-
-
-//?TODO: Fetch the details for each creature
-// let details = [...selector].filter(x => x.textContent.trim()).map(x => {
-//   let key = x.textContent.split(":")[0];
-//   let value = x.textContent.split(":")[1];
-//   return (key === "Social Networks") ? {
-//     [key]: x.lastElementChild.lastElementChild.href
-//   } : { [key]: value }
-// })
 
 
