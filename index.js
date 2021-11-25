23583d053ef8d2a966f07a4d850fc644bf252abe
Jood80 <shawar.nujood@gmail.com>
2021-11-25 22:42:19 +0200

    Chore(*): make the try-catch block wider to hug the whole code

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-const fs = require('fs/promises')
+const fs = require('fs/promises');
 const scrapeArch = require('./architects')
 
 const main = async () => {
-  let curr_limit = 0, max_limit = 30
-  let urls = [], res = []
+  let curr_limit = 0, max_limit = 0, urls = [];
 
-  max_limit = await scrapeArch.getMaxLimit(curr_limit) //TODO: fix this redundant request
-  while (curr_limit <= max_limit) {
-    try {
+  try {
+    max_limit = await scrapeArch.getMaxLimit(curr_limit) //TODO: fix this redundant request
+    while (curr_limit <= max_limit) {
       await scrapeArch.initialize(curr_limit)
       links = await scrapeArch.getLinks('tbody tr')
       urls.push(links.length > 31 ? [... new Set(links)] : links) // Removed Duplicates
 
-      await fs.writeFile('architect_link2.json', JSON.stringify([].concat(...urls), null, 2))
-      curr_limit += 30
-    } catch (error) {
-      console.error(error.message);
-    } finally {
-      await scrapeArch.close()
+      await fs.writeFile('architect_urls3.json', JSON.stringify([].concat(...urls), null, 2))
+      curr_limit += 3
     }
+    await scrapeArch.getDetails([].concat(...urls))
   }
-
-  details = await scrapeArch.getDetails(urls)
-  res.push(details)
-  await fs.writeFile('architect_detail.json', JSON.stringify([].concat(...res), null, 2))
-
+  catch (error) {
+    console.error(error.message);
+  }
+  finally {
+    await scrapeArch.close()
+  }
 }
 
 main()
